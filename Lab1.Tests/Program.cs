63c3484d910a30a790d818e5f8ad9e1a850b1395
Dmitry Ivanov <daug32@mail.ru>
2024-05-21 08:38:14 +0300

    .

--- Lab1.Tests/Program.cs
+++ Lab1.Tests/Program.cs
@@ -5,16 +5,46 @@ namespace Lab1.Tests;
 
 public class Program
 {
-    private static readonly string _testCasesSourceFilePath = @"D:\Development\Projects\TestsCourse\Lab1.Tests\testCasesSource.txt";
-    private static readonly string _testCasesRunResultFilePath = @"D:\Development\Projects\TestsCourse\Lab1.Tests\testCasesRunResult.txt";
-    private static readonly string _programPath = @"D:\Development\Projects\TestsCourse\Lab1.Application\bin\Debug\net8.0\Lab1.Application.exe";
+    private static readonly string _defaultTestCaseSourceFilePath = @"C:\development\projects\qa\Lab1.Tests\testCasesSource.txt";
+    private static readonly string _programPath = @"C:\development\projects\qa\Lab1.Application\bin\Debug\net7.0\Lab1.Application.exe";
 
     public static void Main()
     {
-        var testCasesFileParser = new TestCasesFileParser( _testCasesSourceFilePath );
+        string? testCasesSourceFilePath = AskForTestCasesSourceFilePath();
+        if (testCasesSourceFilePath == null)
+        {
+            return;
+        }
+
+        string testCasesRunResultFilePath = Path.Combine(
+            Path.GetDirectoryName(testCasesSourceFilePath),
+            "testCasesRunResult.txt");
+
+        var testCasesFileParser = new TestCasesFileParser( testCasesSourceFilePath );
         List<CheckTriangleTypeTestCase> testCases = testCasesFileParser.Parse();
         
-        var testRunner = new TestRunner( _testCasesRunResultFilePath, _programPath );
+        var testRunner = new TestRunner( testCasesRunResultFilePath, _programPath );
         testRunner.RunTests( testCases );
+    }
+
+    private static string? AskForTestCasesSourceFilePath()
+    {
+        Console.WriteLine("Введите путь до файла с тест кейсами (ничего для дефолтного значения):");
+        
+        string? testCasesSourceFilePath = Console.ReadLine();
+        if (String.IsNullOrWhiteSpace(testCasesSourceFilePath))
+        {
+            testCasesSourceFilePath = _defaultTestCaseSourceFilePath;
+        }
+
+        testCasesSourceFilePath = Path.GetFullPath(testCasesSourceFilePath);
+
+        if (!File.Exists(testCasesSourceFilePath))
+        {
+            Console.WriteLine($"Файл не существует. FilePath: {testCasesSourceFilePath}");
+            return null;
+        }
+
+        return testCasesSourceFilePath;
     }
 }
